1e7cb7d02c74140ccd8ee6774a61d1092eb3def3
Maik <macgyverx83@gmail.com>
2019-05-14 00:17:13 +0200

    feat(consumer+tests): add consumer and consumer tests

---
+++ src/PactDemo.Consumer.Tests/ConsumerProductPactTestBase.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using PactNet;
+using PactNet.Mocks.MockHttpService;
+
+namespace PactDemo.Consumer.Tests
+{
+    public abstract class ConsumerProductPactTestBase
+    {
+        public int MockServerPort { get; } = 9222;
+
+        public string MockServerBaseUri { get { return $"http://localhost:{MockServerPort}"; } }
+
+        public IMockProviderService MockProviderService { get; private set; }
+
+        public PactBuilder PactBuilder { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Create a PactBuilder instance
+            var pactConfig = new PactConfig()
+            {
+                SpecificationVersion = "2.0.0",
+                PactDir = @"..\..\pacts",
+                LogDir = @"..\..\logs"
+            };
+            PactBuilder = new PactBuilder(pactConfig);
+
+            PactBuilder.ServiceConsumer(typeof(Product).Assembly.GetName().Name).HasPactWith("PactDemo.Provider");
+
+            MockProviderService = PactBuilder.MockService(MockServerPort);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            PactBuilder.Build();
+        }
+    }
+}
---
+++ src/PactDemo.Consumer.Tests/ProductTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using PactNet.Mocks.MockHttpService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PactDemo.Consumer.Tests
+{
+    [TestFixture]
+    public class ProductTests : ConsumerProductPactTestBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            MockProviderService.ClearInteractions();
+        }
+
+        [Test]
+        public async Task GetProduct_ReturnsExpectedProduct()
+        {
+            var expectedProductId = Guid.NewGuid();
+            var expectedProduct = new Product
+            {
+                Id = expectedProductId,
+                Name = "Test",
+                Description = "A product for testing",
+                EanCode = null
+            };
+            MockProviderService
+                .Given("A Product with expected structure") // Describe the state the provider needs to setup
+                .UponReceiving("a GET request for a single product") // textual description - business case
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Get,
+                    Path = $"/{expectedProductId}",
+                })
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 200,
+                    Headers = new Dictionary<string, object>
+                    {
+                        { "Content-Type", "application/json; charset=utf-8" }
+                    },
+                    Body = expectedProduct
+                });
+
+            var consumer = new ProductClient(MockServerBaseUri);
+            var result = await consumer.Get(expectedProductId);
+
+            Assert.AreEqual(expectedProduct.Id, result.Id);
+            Assert.AreEqual(expectedProduct.Name, result.Name);
+
+            MockProviderService.VerifyInteractions();
+        }
+    }
+}
